85d534648089dcf8f13d5132d1e397f676efc213
Sachin Khargie <sachx1281@gmail.com>
2021-10-04 12:33:47 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//starting point of any js app
+
+import react from "react";
+import reactDom from "react-dom";
+
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
